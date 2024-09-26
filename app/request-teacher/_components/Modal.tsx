b7bfbeec20689ab1ec84a4bd6d4d312abd5089ef
clickcafence <69762383+clickcafence@users.noreply.github.com>
2024-09-26 17:14:49 +0300

    Modal

--- app/request-teacher/_components/Modal.tsx
+++ app/request-teacher/_components/Modal.tsx
@@ -51,7 +51,7 @@ const Modal: React.FC<ModalProps> = ({ userId, isOpen, onClose }) => {
           <DialogTitle>Terms of Service Agreement </DialogTitle>
           <DialogDescription>
             Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
+            industry. Lorem Ipsum has been the industry standard dummy text
             ever since the 1500s, when an unknown printer took a galley of type
             and scrambled it to make a type specimen book. It has survived not
             only five centuries, but also the leap into electronic typesetting,
